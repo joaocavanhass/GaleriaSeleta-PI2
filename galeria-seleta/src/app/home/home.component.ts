52242f133914baf6471d39b349f05cf366be1144
joaocavanhas <joaopedro.cavanhascolombo@gmail.com>
2026-04-27 20:03:54 -0300

    criei a home e navbar. inseri a logo, carrosel de produtos, tema, navegacao de produto por peça

--- galeria-seleta/src/app/home/home.component.ts
+++ galeria-seleta/src/app/home/home.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
 
+  slideAtual = 0;
+
+  slides = [
+    { titulo: 'Encontre Peças', italico: 'únicas', subtitulo: 'Descubra tesouro vintage e peças exclusivas' },
+    { titulo: 'Curadoria com',  italico: 'alma',   subtitulo: 'Cada peça carrega uma história única' },
+    { titulo: 'Estilo',         italico: 'atemporal', subtitulo: 'Moda que transcende tendências' },
+  ];
+
+  produtos = [
+    { id: 1, nome: 'Produto 1', descricao: 'Desc. Vintage' },
+    { id: 2, nome: 'Produto 2', descricao: 'Desc. Vintage' },
+    { id: 3, nome: 'Produto 3', descricao: 'Desc. Vintage' },
+    { id: 4, nome: 'Produto 4', descricao: 'Desc. Vintage' },
+  ];
+
+  categorias = ['Roupas', 'Acessórios', 'Decoração', 'Calçados'];
+
+  dropdownAberto = false;
+
+  next() {
+    this.slideAtual = (this.slideAtual + 1) % this.slides.length;
+  }
+
+  prev() {
+    this.slideAtual = (this.slideAtual - 1 + this.slides.length) % this.slides.length;
+  }
 }
